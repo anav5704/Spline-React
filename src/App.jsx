81a424caeb5a7323356d3d487c2dbdf02b5472e5
Anav Chand <anavkki@gmail.com>
2023-09-07 19:49:50 +1200

    Done ✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Spline from '@splinetool/react-spline';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='relative w-screen h-screen'>
+    <div className='b absolute top-0 right-0 w-screen h-screen -z-10'>
+      <nav className=' flex items-center justify-between w-screen p-3 text-white'>
+        <div  className='ml-5 cursor-pointer'>
+          <img className='scale-75' src="/logo.svg" alt="" />
+        </div>
+        <ul className=' flex items-center'>
+          <li className='mr-5 cursor-pointer'>Home</li>
+          <li className='mr-5 cursor-pointer'>About</li>
+          <li className='mr-5 cursor-pointer'>Pricing</li>
+          <button  className='mr-5 cursor-pointer'>Get Started</button>
+        </ul>
+      </nav>
+      <main className='text-white w-1/2  h-80v my-auto flex flex-col justify-center items-start p-5'>
+        <h1 className='font-semibold text-6xl'>The Future is Now</h1>
+        <h3 className='my-5 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, perferendis atque qui nostrum sequi aliquid. Voluptates ipsum nam suscipit quam.</h3>
+        <div className="flex gap-5 items-center">
+          <button className='pri'>Create Account</button>
+          <button>Log In</button>
+        </div>
+      </main>
+    </div>
+    <Spline className='spline' scene="https://prod.spline.design/qZVRVwoO115vt9NN/scene.splinecode" />
+    </div>
+  );
 }
-
-export default App
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      height: {
+				"10v": "10vh",
+				"20v": "20vh",
+				"30v": "30vh",
+				"40v": "40vh",
+				"45v": "45vh",
+				"50v": "50vh",
+				"60v": "60vh",
+				"70v": "70vh",
+				"80v": "80vh",
+				"90v": "90vh",
+				"100v": "100vh",
+			},
+    },
+  },
+  plugins: [],
+}
+
